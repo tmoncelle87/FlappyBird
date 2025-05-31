701ed5d2468837c071fe62f54f6ad6297236cbfb
tmoncelle87 <tmoncelle87@gmail.com>
2025-05-30 21:11:12 -0500

    Major Ui changes
    Lots of ui changes and features

--- flappy/Assets/Scripts/CharacterSelectLogicScript.cs
+++ flappy/Assets/Scripts/CharacterSelectLogicScript.cs
@@ -11,6 +11,23 @@ public class CharacterSelectLogicScript : MonoBehaviour
     {
         
     }
+    public void QuitGame()
+    {
+        // Starts a coroutine to quit the game after a delay. Called by UI buttons.
+        StartCoroutine(DelayedQuit());
+    }
+    private IEnumerator DelayedQuit()
+    {
+        // Provides feedback to the console.
+        Debug.Log("Quitting in 1 second...");
+        yield return new WaitForSeconds(1f); // Waits for 1 second.
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // Stops play mode in the editor.
+#else
+        Application.Quit(); // Quits the application in a build.
+#endif
+    }
 
     // Update is called once per frame
     void Update()
---
+++ flappy/Assets/Scripts/ObjectJitterScripts.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectJitterScripts : MonoBehaviour
+{
+    private RectTransform ObjectImage;
+
+
+    public float speed;
+    public float leftLimit;
+    public float rightLimit;
+    private bool movingRight;
+
+    void Start()
+    {
+        ObjectImage = GetComponent<RectTransform>();
+        Vector2 pos = ObjectImage.anchoredPosition;
+        if (pos.x > rightLimit)
+        {
+            movingRight = false;
+        }
+        else if (pos.x < leftLimit)
+        {
+            movingRight = true;
+        }
+    }
+
+    void Update()
+    {
+        Vector2 pos = ObjectImage.anchoredPosition;
+
+
+
+
+        if (movingRight)
+        {
+            pos.x += speed * Time.deltaTime;
+            if (pos.x >= rightLimit)
+            {
+                pos.x = rightLimit;
+                movingRight = false;
+            }
+        }
+        else
+        {
+            pos.x -= speed * Time.deltaTime;
+            if (pos.x <= leftLimit)
+            {
+                pos.x = leftLimit;
+                movingRight = true;
+            }
+        }
+
+        ObjectImage.anchoredPosition = pos;
+    }
+}
